e70808b27b0e5572ae2b51703accd36c90406f0f
BettyShawla <betty.tada1@gmail.com>
2022-10-13 14:28:56 -0400

    Wild Wild Life answered

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ for(let k = 1; k <=100; k++){
     }
     console.log(k)
 }
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee.splice(2, 1, 5001)
+console.log(plantee)
+
+wolfy.splice(3,1, "Gotham City")
+console.log(wolfy)
+
+dart.push("Hawkins")
+console.log(dart)
+
+wolfy[0] = "Gameboy"
+console.log(wolfy)
